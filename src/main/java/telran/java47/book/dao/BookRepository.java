136d4cb8a24fe57dc9e0dddcbeef37b209dbc059
KhlebnikovEvgeniy <khebnikove@gmail.com>
2023-07-09 22:31:13 +0300

    Repositories: BookRepository

--- src/main/java/telran/java47/book/dao/BookRepository.java
+++ src/main/java/telran/java47/book/dao/BookRepository.java
@@ -1,9 +1,18 @@
 package telran.java47.book.dao;
 
+import java.util.stream.Stream;
+
 import org.springframework.data.repository.PagingAndSortingRepository;
 
+import telran.java47.book.model.Author;
 import telran.java47.book.model.Book;
+import telran.java47.book.model.Publisher;
+
 
 public interface BookRepository extends PagingAndSortingRepository<Book, String> {
+
+	Stream<Book> findAllByAuthors(Author author);
+	
+	Stream<Book> findAllByPublisher(Publisher publisher);
 
 }
