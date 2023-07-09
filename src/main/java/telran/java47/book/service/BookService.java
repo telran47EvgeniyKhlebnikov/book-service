c33bb1521b7c32d750fe9817925fefef79ac0de1
KhlebnikovEvgeniy <khebnikove@gmail.com>
2023-07-09 21:13:26 +0300

    Service: BookService

--- src/main/java/telran/java47/book/service/BookService.java
+++ src/main/java/telran/java47/book/service/BookService.java
@@ -1,5 +1,9 @@
 package telran.java47.book.service;
 
+
+import java.util.Set;
+
+import telran.java47.book.dto.AuthorDto;
 import telran.java47.book.dto.BookDto;
 
 public interface BookService {
@@ -7,5 +11,19 @@ public interface BookService {
 	boolean addBook(BookDto bookDto);
 	
 	BookDto findBookByIsbn(String isbn);
+	
+	BookDto removeBook(String isbn);
+	
+	BookDto updateBookTitle(String isbn, String updatedTitle);
+	
+	Set<BookDto> findBooksByAuthor(String authorName);
+	
+	Set<BookDto> findBooksByPublisher(String publisherName);
+	
+	Set<AuthorDto> findBookAuthors(String isbn);
+	
+	Set<String> findPublishersByAuthor(String authorName);
+	
+	AuthorDto removeAuthor(String authorName);
 
 }
