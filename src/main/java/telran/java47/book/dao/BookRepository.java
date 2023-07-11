008d5da6d3c15e0843537750cf0d1e9aa77c19a3
KhlebnikovEvgeniy <khebnikove@gmail.com>
2023-07-11 20:17:21 +0300

    Bidirectional associations

--- src/main/java/telran/java47/book/dao/BookRepository.java
+++ src/main/java/telran/java47/book/dao/BookRepository.java
@@ -7,10 +7,9 @@ import org.springframework.data.repository.PagingAndSortingRepository;
 import telran.java47.book.model.Book;
 
 public interface BookRepository extends PagingAndSortingRepository<Book, String> {
+	
 	Stream<Book> findByAuthorsName(String name);
 
 	Stream<Book> findByPublisherPublisherName(String name);
 	
-	
-
 }
--- src/main/java/telran/java47/book/dao/PublisherRepository.java
+++ src/main/java/telran/java47/book/dao/PublisherRepository.java
@@ -14,4 +14,5 @@ public interface PublisherRepository extends PagingAndSortingRepository<Publishe
 	List<String> findPublishersByAuthor(String authorName);
 	
 	Stream<Publisher> findDistinctByBooksAuthorsName(String authorName);
+	
 }
